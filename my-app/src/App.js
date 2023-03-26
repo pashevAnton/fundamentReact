80df8dd1e61afd13733d4fd1aab6e1e21a74fb86
PagilayaSkumbria54 <toni.pashev@bk.ru>
2023-03-27 01:13:36 +0300

    Add sorting select

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,16 @@ import React, {useState} from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'},
+        {id: 1, title: 'Python', body: 'Add something'},
+        {id: 2, title: 'Javascript', body: 'Description'},
+        {id: 3, title: 'Kotlin', body: 'Write description'},
     ])
+
+    const [selectedSort, setSelectedSort] = useState('')
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -18,11 +21,28 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts([...posts].sort((a,b) =>
+            a[sort].localeCompare(b[sort])
+        ))
+
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
             <div>
-
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка"
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'},
+                    ]}
+                />
             </div>
             {posts.length
                 ? <PostList remove={removePost} posts={posts} title="Посты про JS"/>
