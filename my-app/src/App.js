dc4c02aabad7b40167fdc21f7b2bdeb692834d36
PagilayaSkumbria54 <toni.pashev@bk.ru>
2023-04-09 21:36:39 +0300

    Add pagination

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,29 +5,33 @@ import PostForm from "./PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
-import {useSortedPosts} from "./hooks/usePosts";
+import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount} from "./components/UI/utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Python', body: 'Add something'},
-        {id: 2, title: 'Javascript', body: 'Description'},
-        {id: 3, title: 'Kotlin', body: 'Write description'},
-    ])
-
+    const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-    const sortedAndSearchedPosts = useSortedPosts(posts, filter.sort, filter.query)
-    const [fetchPosts, isPostLoading, postError] = useFetching(async() => {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+
+    const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
     })
 
     useEffect(() => {
         fetchPosts()
-    }, [])
+    }, [page])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -36,6 +40,10 @@ function App() {
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page)
     }
 
     return (
@@ -53,12 +61,17 @@ function App() {
             />
             {postError && <h1>Произошла ошибка: ${postError}</h1>}
             {isPostLoading
-            ?   <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
                     <Loader/>
                 </div>
-            :
+                :
                 <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
             }
+            <Pagination
+                page={page}
+                changePage={changePage}
+                totalPages={totalPages}
+            />
         </div>
     );
 }
