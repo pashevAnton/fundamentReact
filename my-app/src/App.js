dd4b2ed3dc8be76c6b999124014abe03d2608bdc
PagilayaSkumbria54 <toni.pashev@bk.ru>
2023-04-09 17:55:13 +0300

    Create custom hook usePosts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import React, {useMemo, useState} from "react";
+import React, {useState} from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {useSortedPosts} from "./hooks/usePosts";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -15,6 +16,7 @@ function App() {
 
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = useSortedPosts(posts, filter.sort, filter.query)
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -24,19 +26,6 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
-
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) =>
-                a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    }, [filter.sort, posts])
-
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
 
     return (
         <div className="App">
